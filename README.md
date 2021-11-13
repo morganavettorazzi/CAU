4c0af8ce0ab9d6722d87738a5f71eb4fb9283343
Morgana Vettorazzi <morgana@buscaepi.com>
2021-11-13 19:56:12 -0300

    Inicializacao do projeto

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# CAU
-Repositório para realização do trabalho de Dev. Web (Central de Atendimento ao Usuário).
+# cau (central-de-atendimento-ao-usuario)
+
+central de atendimento ao usuario
+
+## Install the dependencies
+```bash
+npm install
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+```bash
+quasar dev
+```
+
+
+### Build the app for production
+```bash
+quasar build
+```
+
+### Customize the configuration
+See [Configuring quasar.conf.js](https://quasar.dev/quasar-cli/quasar-conf-js).
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="q-pa-md">
+    <q-layout view="hHh Lpr lff" >
+      <q-header elevated class="degrade">
+        <q-toolbar>
+          <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
+          <q-toolbar-title>Olá Morgana</q-toolbar-title>
+        </q-toolbar>
+      </q-header>
+
+      <q-drawer
+        v-model="drawer"
+        show-if-above
+
+        :mini="miniState"
+        @mouseover="miniState = false"
+        @mouseout="miniState = true"
+        mini-to-overlay
+
+        :width="200"
+        :breakpoint="500"
+        bordered
+        class="bg-deep-purple-1"
+      >
+        <q-scroll-area class="fit">
+          <q-list padding>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="dashboard" />
+              </q-item-section>
+
+              <q-item-section>
+                Dashboard
+              </q-item-section>
+            </q-item>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="comment" />
+              </q-item-section>
+
+              <q-item-section>
+                Chamados
+              </q-item-section>
+            </q-item>
+           
+          </q-list>
+        </q-scroll-area>
+      </q-drawer>
+
+      <q-page-container>
+        <q-page padding>
+          <p v-for="n in 15" :key="n">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil praesentium molestias a adipisci, dolore vitae odit, quidem consequatur optio voluptates asperiores pariatur eos numquam rerum delectus commodi perferendis voluptate?
+          </p>
+        </q-page>
+      </q-page-container>
+    </q-layout>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      drawer: ref(false),
+      miniState: ref(true)
+    }
+  }
+}
+</script>
+<style scoped>
+.degrade{
+  background: rgb(17,27,109);
+  background: linear-gradient(90deg, rgba(17,27,109,1) 0%, rgba(59,24,92,1) 0%, rgba(35,18,65,1) 100%);
+}
+</style>
